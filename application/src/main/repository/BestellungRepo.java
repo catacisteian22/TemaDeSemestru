32a44456ad0faee99ec760f8586868376779d432
elenabachis <133968356+elenabachis@users.noreply.github.com>
2023-10-30 22:06:03 +0200

    Changes by catac

--- application/src/main/repository/BestellungRepo.java
+++ application/src/main/repository/BestellungRepo.java
@@ -1,4 +1,58 @@
 package main.repository;
 
-public class BestellungRepo {
+import main.model.Bestellung;
+import main.model.Buch;
+
+import java.util.List;
+
+public class BestellungRepo implements InMemoryRepo<Bestellung> {
+
+    List<Bestellung> bestellungList;
+
+    public BestellungRepo(List<Bestellung> bestellungList) {
+
+        this.bestellungList = bestellungList;
+    }
+
+    /**
+     * @param b
+     */
+//    @Override
+    public void add(Bestellung b){
+        bestellungList.add(b);
+    }
+
+    //    @Override
+    public Bestellung getById(String id) {
+        for (Bestellung bestellung : bestellungList) {
+            if (bestellung.getIdBestellung() == id) {
+                return bestellung;
+            }
+        }
+        return null;
+    }
+
+    public void delete(String id) {
+        for (Bestellung bestellung : bestellungList) {
+            if (bestellung.getIdBestellung() == id) {
+                bestellungList.remove(bestellung);
+            }
+        }
+
+    }
+
+    public Bestellung update(String id, Bestellung newBestellung) {
+        for (int i = 0; i < bestellungList.size(); i++) {
+            Bestellung bestellung = bestellungList.get(i);
+            if (bestellung.getIdBestellung().equals(id)) {
+                bestellungList.set(i, newBestellung);
+                return newBestellung;
+            }
+        }
+        return null;
+    }
+
+    public List<Bestellung> getAll() {
+        return bestellungList;
+    }
 }
